1c6ae03c9e8a701ad68f8482ccf77c4505a375e4
vitaliiSh <vitalii.shkar@gmail.com>
2023-12-01 18:47:22 +0200

    Data output, filtering, search

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,34 +1,63 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-lg-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+            `
+
+            goodsContainer.append(goodBlock)
+        })
+    }
+
+    const getData = (value, category) => {
         fetch('/db/db.json')
             .then((responce) => responce.json())
             .then((data) => {
-                localStorage.setItem('goods', JSON.stringify(data))
+                const array = category ? data.filter(item => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array))
+
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array)
+                }
             })
     }
 
     links.forEach(link => {
         link.addEventListener('click', event => {
             event.preventDefault()
-            getData()
-            console.log(localStorage);
-            localStorage.removeItem('goods')
-            console.log(localStorage);
+            const linkValue = link.textContent
+            const category = link.dataset.field
+
+            getData(linkValue, category)
         })
     })
 
-    // localStorage.setItem('goods', JSON.stringify([1,2,3,4,5]))
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
 
-    // const goods = JSON.parse(localStorage.getItem('goods'))
-
-    // console.log(goods);
-    // console.log(localStorage);
-
-    // localStorage.removeItem('goods')
-
-    // console.log(localStorage);
 }
 
 getGoods()
