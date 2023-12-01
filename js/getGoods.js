876b3fe558b705c77407e45115c1a1f16f6eb9cf
vitaliiSh <vitalii.shkar@gmail.com>
2023-12-01 15:35:36 +0200

    receive data from local DB

---
+++ js/getGoods.js
@@ -0,0 +1,31 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link')
+
+    const getData = () => {
+        fetch('/db/db.json')
+            .then((responce) => responce.json())
+            .then((data) => {
+                console.log(data);
+            })
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault()
+            getData()
+        })
+    })
+
+    localStorage.setItem('goods', JSON.stringify([1,2,3,4,5]))
+
+    const goods = JSON.parse(localStorage.getItem('goods'))
+
+    console.log(goods);
+    console.log(localStorage);
+
+    localStorage.removeItem('goods')
+
+    console.log(localStorage);
+}
+
+getGoods()
