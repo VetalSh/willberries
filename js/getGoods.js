9a5390813d2cdb061df0ae755a5a475b46a79c07
vitaliiSh <vitalii.shkar@gmail.com>
2023-12-05 12:36:19 +0200

    Cart

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
+    const more = document.querySelector('.more')
 
     const renderGoods = (goods) => {
         const goodsContainer = document.querySelector('.long-goods-list')
@@ -14,14 +15,14 @@ const getGoods = () => {
 
             goodBlock.innerHTML = `
                 <div class="goods-card">
-						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
-						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
-						<h3 class="goods-title">${good.name}</h3>
-						<p class="goods-description">${good.description}</p>
-						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
-							<span class="button-price">$${good.price}</span>
-						</button>
-					</div>
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
             `
 
             goodsContainer.append(goodBlock)
@@ -56,6 +57,13 @@ const getGoods = () => {
 
     if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
         renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
+
+    if (more) {
+        more.addEventListener('click', () => {
+            event.preventDefault()
+            getData()
+        })
     }
 
 }
